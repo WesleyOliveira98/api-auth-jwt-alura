b0cc38b57dc6283f6d1057121dac6c12db96d7e7
Wesley <wesleygabrieldeoliveira@gmail.com>
2022-03-11 14:31:00 -0300

    Criação das Estratégias de Autenticação usando o passport; Criação da rota de login

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const estrategiasAuth = require('./src/usuarios');
 
 app.use(
   bodyParser.urlencoded({
